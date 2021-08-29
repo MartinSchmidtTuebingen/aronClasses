bcb625c2086ff45892b2f4c809b48b7333865bec
MartinSchmidtTuebingen <33291683+MartinSchmidtTuebingen@users.noreply.github.com>
2021-08-29 20:49:18 +0200

    Add more functionality for User Class

--- Model/User.php
+++ Model/User.php
@@ -2,28 +2,61 @@
 
 namespace Aron\Model;
 
-use \Aron\Core\DatabaseProvider;
+use Aron\Core\DatabaseProvider;
+use Aron\Model\BaseModel;
 use PDO;
 
-class User
+class User extends BaseModel
 {
+    protected $baseTable = 'user_daten';
+
+    protected $properties = [
+        'IDX' => '',
+        'usr_name' => '',
+        'usr_mail' => '',
+        'usr_pw' => '',
+        'usr_aktiv' => '',
+        'usr_salt' => '',
+    ];
+
     private $pepper = '69cd9406ce7ea11fecbafae9f7e9d44e';
 
-    public function checkPassword(?string $userName, ?string $password): bool
+    public function loadWithName(?string $userName)
     {
-        if (is_null($userName) || is_null($password) || $userName == '' || $password == '') {
+        if (is_null($userName) || $userName == '') {
             return false;
         }
 
-        $db = DatabaseProvider::getDb();
+        $getIndexStatement = DatabaseProvider::getDb()->prepare("SELECT IDX FROM $this->baseTable WHERE usr_mail = ? LIMIT 1");
+        $getIndexStatement->execute([$userName]);
+        $userId = $getIndexStatement->fetch()[0];
 
-        $getPasswordStatement = $db->prepare('SELECT usr_pw, usr_salt FROM user_daten WHERE usr_mail = ? LIMIT 1');
-        $getPasswordStatement->execute([$userName]);
-        $userData = $getPasswordStatement->fetch(PDO::FETCH_ASSOC);
+        return $this->load($userId);
+    }
 
-        $passwordHash = $userData['usr_pw'];
+    public function checkPassword(?string $password, ?string $userName): bool
+    {
+        if (is_null($password) || $password == '') {
+            return false;
+        }
 
-        if ($passwordHash === $this->getPasswordHash($password, $userData['usr_salt'])) {
+        if (!$this->isLoaded) {
+            if (is_null($userName) || $userName == '') {
+                return false;
+            }
+
+            $getPasswordStatement = DatabaseProvider::getDb()->prepare("SELECT usr_pw, usr_salt FROM $this->baseTable WHERE usr_mail = ? LIMIT 1");
+            $getPasswordStatement->execute([$userName]);
+            $userData = $getPasswordStatement->fetch(PDO::FETCH_ASSOC);
+
+            $passwordHash = $userData['usr_pw'];
+            $salt = $userData['usr_salt'];
+        } else {
+            $passwordHash = $this->properties['usr_pw'];
+            $salt = $this->properties['usr_salt'];
+        }
+
+        if ($passwordHash === $this->getPasswordHash($password, $salt)) {
             return true;
         } else {
             return false;
@@ -33,5 +66,10 @@ class User
     private function getPasswordHash(string $userPassword, string $salt): string
     {
         return md5($salt . $userPassword . $this->pepper);
+    }
+
+    public function getUserName(): string
+    {
+        return $this->properties['usr_name'];
     }
 }
