819502f3ba1137e4a6abc1e5346bf5a797e193ae
MartinSchmidtTuebingen <33291683+MartinSchmidtTuebingen@users.noreply.github.com>
2021-09-04 01:59:18 +0200

    Create register function in controller to create a new user

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -2,7 +2,67 @@
 
 namespace Aron\Controller;
 
+use Aron\Core\Registry;
+
 class RegisterController extends FrontendController
 {
     protected $content = "./tpl/register.tpl";
+
+    public function register()
+    {
+        $request = Registry::getRequest();
+        $userName = $request->getParameter('username');
+        $mailAdress = $request->getParameter('mailadress');
+        $mailAdressValidation = $request->getParameter('mailadressvalidation');
+        $password = $request->getParameter('password');
+        $passwordValidation = $request->getParameter('passwordvalidation');
+
+        if (!$this->validateUserName($userName)) {
+            echo "Choose other userName";
+        }
+
+        if (!$this->validateMailAdress($mailAdress, $mailAdressValidation)) {
+            echo "Choose other mailAdress or they dont match";
+        }
+
+        if (!$this->validatePassword($password, $passwordValidation)) {
+            echo "Choose other password or they dont match";
+        }
+
+        $user = new User();
+        if ($user->registerUser($userName, $mailAdress, $password) {
+            //Send activation mail
+        } else {
+            echo "Something went wrong in the registration";
+        }
+
+        return 'account';
+    }
+
+    private function validateUserName(?string $userName)
+    {
+        if ($userName == '') {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function validateMailAdress($mailAdress, $mailAdressValidation)
+    {
+        if ($mailAdress != $mailAdressValidation) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function validateMailAdress($password, $passwordValidation)
+    {
+        if ($password != $passwordValidation) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- tpl/register.tpl
+++ tpl/register.tpl
@@ -1,5 +1,5 @@
 <p>
-<form action='index.php?cl=account&fnc=register' method='post' autocomplete='off'
+<form action='index.php?cl=register&fnc=register' method='post' autocomplete='off'
                      onsubmit="if(this.password.value != this.password2.value)
                 {alert('Passworteingabe unterscheidet sich!');
                  return false;}">
@@ -14,19 +14,19 @@
 <td>
         <label>Mailadresse</td><td> <input type='text' name='mailadress' placeholder='mustermensch@mustermail.de' required/></label></td></tr>
 		<td>
-        <label>Best&auml;tige Mailadresse</td><td> <input type='text' name='maildress2'placeholder='mustermensch@mustermail.de' required/></label></td></tr>
+        <label>Best&auml;tige Mailadresse</td><td> <input type='text' name='mailadressvalidation'placeholder='mustermensch@mustermail.de' required/></label></td></tr>
 <tr>
 <td>
         <label>Passwort</td><td><input type='password' name='password' placeholder='Passwort' required//></br></label></td></tr>
-  
+
   <tr>
   <tr>
 <td>
-        <label>Best&auml;tige Passwort</td><td><input type='password' name='password2' placeholder='Passwort' required//></br></label></td></tr>
-  
+        <label>Best&auml;tige Passwort</td><td><input type='password' name='passwordvalidation' placeholder='Passwort' required//></br></label></td></tr>
+
   <tr>
 <td>      <input type='submit' name='formaction' value='OK'/></td></tr></table>
     </fieldset>
 </form>
- 
+
 </p>
